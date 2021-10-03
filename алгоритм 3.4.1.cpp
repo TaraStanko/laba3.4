67722a374fb7649edb6204ba4cc1353a8a69990f
TaraStanko <91203149+TaraStanko@users.noreply.github.com>
2021-10-03 17:17:31 +0300

    Add files via upload

---
+++ алгоритм 3.4.1.cpp
@@ -0,0 +1,26 @@
+﻿#include<cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;// вхідний аргумент
+	double y;// вхідний параметр
+	double R1;//радіус більшого кола
+	double R2;//радіус меншого кола
+	const float Pi = 3.1415;
+	cout << " x = ";cin >> x;
+	cout << " y = ";cin >> y;
+	cout << " R1 = ";cin >> R1;
+	cout << " R2 = ";cin >> R2;
+	// розгалуження в повній формі
+	if ((y > 0 && y <= Pi * pow(R2, 2) && y <= x) ||
+		(y < 0 && y <= Pi * pow(R1, 2) && y >= Pi * pow(R2, 2) && y >= -x))
+		cout << "Yes" << endl;
+	else
+		cout <<"No" << endl;
+
+	cin.get();
+	return 0;
+}
